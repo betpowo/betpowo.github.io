b5d5532c2f04c2a5c9556e1ede40876bcd4e67a2
Betopia <73242725+betpowo@users.noreply.github.com>
2024-07-12 21:21:33 -0400

    gallery and shit (wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [alpine()],
+	integrations: [alpine(), mdx()],
 	site: 'https://betpowo.github.io'
 });
