b20deeafedd7332c06e4f87995ca3a65e9aa1ae9
RetiredArchive <146268415+RetiredArchive@users.noreply.github.com>
2024-08-22 13:49:33 -0500

    something i think

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+	redirects: {
+		'/betty': '/characters/betty',
+		'/josephine': '/characters/josephine',
+		'/glib': '/characters/glib',
+		'/betpo': '/characters/betpo',
+		'/carlos': '/characters/carlos',
+	},
 	integrations: [alpine(), mdx()],
 	site: 'https://betpowo.github.io'
 });
